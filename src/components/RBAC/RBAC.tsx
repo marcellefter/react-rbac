abd5b47bab769908c06cf4665e0d10ce1f5e5b22
Marcel Lefter <marcel.lefter@ebs-integrator.com>
2021-03-25 22:00:42 +0200

    Added RBAC component

---
+++ src/components/RBAC/RBAC.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RBAC: React.FC = ({ children }) => {
+  return <div className="rbac">{children}</div>;
+};
+
+export default RBAC;
---
+++ src/components/RBAC/index.ts
@@ -0,0 +1,1 @@
+export { default as RBAC } from "./RBAC";
